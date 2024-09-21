58460f1e11fd7f1cb2b282d0080cbb71d7cfbc09
odoodjinn <odoodjinn@gmail.com>
2024-09-21 17:08:18 +0530

    Update purchase_order.py

--- average_cost/models/purchase_order.py
+++ average_cost/models/purchase_order.py
@@ -5,19 +5,3 @@ from odoo import models
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-
-
-"""/** @odoo-module */
-
-//for adding rating fields to props
-import { patch } from "@web/core/utils/patch";
-import { Orderline } from "@point_of_sale/app/store/models";
-patch(Orderline.prototype, {
-   getDisplayData() {
-       return {
-           ...super.getDisplayData(...arguments),
-            rating: this.product.rating,
-       };
-   },
-});
-"""
