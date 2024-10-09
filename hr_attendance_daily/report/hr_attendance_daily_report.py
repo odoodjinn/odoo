4277cfee456c2aa216fff0a86a9c457864e6fee3
odoodjinn <odoodjinn@gmail.com>
2024-10-09 12:07:48 +0530

    daily attendance module completed

--- hr_attendance_daily/report/hr_attendance_daily_report.py
+++ hr_attendance_daily/report/hr_attendance_daily_report.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-from datetime import date, datetime
 
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
-from odoo.tools import date_utils
 
 
 class HrAttendanceDailyReport(models.AbstractModel):
@@ -14,13 +12,35 @@ class HrAttendanceDailyReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         """ Fetch data from the wizard model and attendance model,
         and then return to the qweb template to print the pdf."""
-        attendance_obj = self.env['hr.attendance'].search([
-            ('check_in', '>=', data['start_date']),
-            # ('check_out', '<=', data['end_date']),
-        ])
-        print(data, '//data')
-        if not attendance_obj:
-            raise ValidationError('No such records found!')
+        if data['print_by'] == 'daily':
+            attendance_obj = self.env['hr.attendance'].search([
+                ('check_in', '>=', data['today']),
+                ('check_out', '<=', data['today']),
+            ])
+            if not attendance_obj:
+                raise ValidationError('No such records found!')
+
+        elif data['print_by'] == 'weekly':
+            attendance_obj = self.env['hr.attendance'].search([
+                ('check_in', '>=', data['starting_week']),
+                ('check_out', '<=', data['ending_week']),
+            ])
+            if not attendance_obj:
+                raise ValidationError('No such records found!')
+        elif data['print_by'] == 'monthly':
+            attendance_obj = self.env['hr.attendance'].search([
+                ('check_in', '>=', data['starting_month']),
+                ('check_out', '<=', data['ending_month']),
+            ])
+            if not attendance_obj:
+                raise ValidationError('No such records found!')
+        else:
+            attendance_obj = self.env['hr.attendance'].search([
+                ('check_in', '>=', data['start_date']),
+                ('check_out', '<=', data['end_date']),
+            ])
+            if not attendance_obj:
+                raise ValidationError('No such records found!')
         return {
             'docs': attendance_obj,
         }
--- hr_attendance_daily/wizard/hr_attendance_daily_wizard.py
+++ hr_attendance_daily/wizard/hr_attendance_daily_wizard.py
@@ -8,20 +8,28 @@ class HrAttendanceDailyWizard(models.TransientModel):
     _name = 'hr.attendance.daily.wizard'
     _description = 'Daily Attendance Report Wizard'
 
+    print_by = fields.Selection([('daily', 'Daily'),('weekly', 'Weekly'), ('monthly', 'Monthly'), ('custom', 'Custom')],string='Print By', default='daily')
     start_date = fields.Date(string='Start Date')
     end_date = fields.Date(string='End Date')
-    print_by = fields.Selection([('daily', 'Daily'),('weekly', 'Weekly'), ('monthly', 'Monthly')])
 
     def action_print_report(self):
         """Print button action on the wizard to pass the filter data to the abstract model
             and print the pdf"""
-        start_date = self.start_date
-        starting_week = date_utils.start_of(start_date, "week")
-        print(starting_week, 'starting week')
+        today = fields.Date.today()
+        starting_week = date_utils.start_of(today, "week")
+        ending_week = date_utils.end_of(today, "week")
+        starting_month = date_utils.start_of(today, "month")
+        ending_month = date_utils.end_of(today, "month")
+
         data = {
-            'start_date': start_date,
-            'end_date': self.end_date,
+            'today': today,
             'print_by': self.print_by,
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'starting_week': starting_week,
+            'ending_week': ending_week,
+            'starting_month': starting_month,
+            'ending_month': ending_month,
         }
         return self.env.ref('hr_attendance_daily.action_report_hr_attendance_daily').report_action(
             None, data=data)
